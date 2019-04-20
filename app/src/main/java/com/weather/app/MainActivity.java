f21f30d9fac5fe7f5aea121db83801d737e5bf0c
riddhesh-mahajan <riddheshgp@gmail.com>
2019-04-20 08:25:11 +0530

    Shortcut added for mumbai weather

--- app/src/main/java/com/weather/app/MainActivity.java
+++ app/src/main/java/com/weather/app/MainActivity.java
@@ -6,12 +6,17 @@ import android.net.ConnectivityManager;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
+
+public class MainActivity extends AppCompatActivity{
     private Button show_weather_button;
+    private String TAG = "LOGMain";
+    private static final int LOCATION_SETTING_REQUEST_CODE = 3;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -29,10 +34,7 @@ public class MainActivity extends AppCompatActivity {
                     String longitude_input_value = longitude_input.getText().toString();
 
                     if (latitude_input_value.length() != 0 && longitude_input_value.length() != 0) {
-                        Intent i = new Intent(getApplicationContext(), weather.class);
-                        i.putExtra("latitude", latitude_input_value);
-                        i.putExtra("longitude", longitude_input_value);
-                        startActivity(i);
+                        startWeatherActivity(latitude_input_value,longitude_input_value);
                     }else{
                         Snackbar snackbar = Snackbar.make(findViewById(R.id.main_layout),
                                 "Enter latitude and longitude",
@@ -48,6 +50,31 @@ public class MainActivity extends AppCompatActivity {
             }
         };
         show_weather_button.setOnClickListener(show_weather_onclick_listener);
+
+
+        //Shortcuts
+        Button mumbai_weather_button = findViewById(R.id.show_mumbai_weather_button);
+
+        View.OnClickListener show_mumbai_weather_onclick_listener = new View.OnClickListener() {
+            public void onClick(View v) {
+                if(isNetworkConnected()) {
+                    startWeatherActivity("19.0760","72.8777");
+                }else{
+                    Snackbar snackbar = Snackbar.make(findViewById(R.id.main_layout),
+                            "No internet connection",
+                            Snackbar.LENGTH_LONG);
+                    snackbar.show();
+                }
+            }
+        };
+        mumbai_weather_button.setOnClickListener(show_mumbai_weather_onclick_listener);
+    }
+
+    void startWeatherActivity(String latitude_input_value,String longitude_input_value){
+        Intent i = new Intent(getApplicationContext(), weather.class);
+        i.putExtra("latitude", latitude_input_value);
+        i.putExtra("longitude", longitude_input_value);
+        startActivity(i);
     }
 
     private boolean isNetworkConnected() {
@@ -55,4 +82,6 @@ public class MainActivity extends AppCompatActivity {
 
         return cm.getActiveNetworkInfo() != null;
     }
+
+
 }
